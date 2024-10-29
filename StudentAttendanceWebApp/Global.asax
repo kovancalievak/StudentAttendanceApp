3c095b824bf8e5d9baf10c84a9a5da553a18749e
Klimentina <kovancalieva.k@gmail.com>
2024-10-29 18:07:59 +0100

    first commit

---
+++ StudentAttendanceWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentAttendanceWebApp.MvcApplication" Language="C#" %>
---
+++ StudentAttendanceWebApp/Models/Lesson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentAttendanceWebApp.Models
+{
+    public class Lesson
+    {
+        [Key]
+        public int lessonNum { get; set; }
+        public string lessonDate { get; set; }
+        public string studentsAttendance { get; set; }  
+    }
+}
